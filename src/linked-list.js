e2eca6ffeadc28dfe3f8b847ed03a8c2710e2b36
Tatyana Shulha <tsulga6@gmail.com>
2019-10-03 11:53:31 +0300

    feat: add intermediate option

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,9 +1,36 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.head = null; 
+        this.size = 0; 
+    }
 
-    append(data) {}
+    append(data) {
+        // creates a new node 
+    var node = new Node(element); 
+  
+    // to store current node 
+    var current; 
+  
+    // if list is Empty add the 
+    // element and make it head 
+    if (this.head == null) 
+        this.head = node; 
+    else { 
+        current = this.head; 
+  
+        // iterate to the end of the 
+        // list 
+        while (current.next) { 
+            current = current.next; 
+        } 
+  
+        // add node 
+        current.next = node; 
+    } 
+    this.size++; 
+    }
 
     head() {}
 
@@ -11,17 +38,112 @@ class LinkedList {
 
     at(index) {}
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        if (index > 0 && index > this.size) 
+        return false; 
+    else { 
+        // creates a new node 
+        var node = new Node(element); 
+        var curr, prev; 
+  
+        curr = this.head; 
+  
+        // add the element to the 
+        // first index 
+        if (index == 0) { 
+            node.next = head; 
+            this.head = node; 
+        } else { 
+            curr = this.head; 
+            var it = 0; 
+  
+            // iterate over the list to find 
+            // the position to insert 
+            while (it < index) { 
+                it++; 
+                prev = curr; 
+                curr = curr.next; 
+            } 
+  
+            // adding an element 
+            node.next = curr; 
+            prev.next = node; 
+        } 
+        this.size++; 
+    } 
+    }
 
-    isEmpty() {}
+    isEmpty() {
+        return this.size == 0; 
+    }
 
-    clear() {}
+    clear() {
+        var current = this.head; 
+    var prev = null; 
+  
+    // iterate over the list 
+    while (current != null) { 
+        // comparing element with current 
+        // element if found then remove the 
+        // and return true 
+        if (current.element === element) { 
+            if (prev == null) { 
+                this.head = current.next; 
+            } else { 
+                prev.next = current.next; 
+            } 
+            this.size--; 
+            return current.element; 
+        } 
+        prev = current; 
+        current = current.next; 
+    } 
+    return -1; 
+    }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        var current = this.head; 
+        var prev = null; 
+  
+    // iterate over the list 
+    while (current != null) { 
+        // comparing element with current 
+        // element if found then remove the 
+        // and return true 
+        if (current.element === element) { 
+            if (prev == null) { 
+                this.head = current.next; 
+            } else { 
+                prev.next = current.next; 
+            } 
+            this.size--; 
+            return current.element; 
+        } 
+        prev = current; 
+        current = current.next; 
+    } 
+    return -1; 
+    }
 
     reverse() {}
 
-    indexOf(data) {}
+    indexOf(data) {
+        var count = 0; 
+    var current = this.head; 
+  
+    // iterae over the list 
+    while (current != null) { 
+        // compare each element of the list 
+        // with given element 
+        if (current.element === element) 
+            return count; 
+        count++; 
+        current = current.next; 
+    } 
+  
+    // not found 
+    return -1; 
+    }
 }
 
 module.exports = LinkedList;
